64fa8b16153b6ec6976a4fc6a0cf5ccbf5f7a142
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-05-09 23:06:14 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Logo from '../assets/logo.jpg'
 
 const Navbar = () => {
   return (
     <>
     <nav >
-      
+      <div
+      className='h-16 flex items-center justify-between px-4'>
+      <img className='h-20 ' src={Logo} alt="Logo" srcset="" />
+      <div className='flex space-x-4'>
+        <a href="/Home">Home</a>
+        <a href="/About">About</a>
+        <a href="/Contact">Contact</a>
+      </div>
+      </div>
+
       
     </nav>
     
